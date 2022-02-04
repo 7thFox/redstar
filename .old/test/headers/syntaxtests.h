7c68a7a4bede81160c11c1922fda9fc04045e142
7thFox <dev7thFox@gmail.com>
2022-02-03 22:12:10 -0600

    delete old C code

--- .old/test/headers/syntaxtests.h
+++
@@ -1,9 +0,0 @@
-#ifndef SYNTAXTESTS_H
-#define SYNTAXTESTS_H
-
-#include "testcommon.h"
-
-TestResult success_string_literal();
-TestResult success_string_literal_escapes();
-
-#endif
--- .old/test/syntaxtests.c
+++
@@ -1,8 +0,0 @@
-#include "headers/syntaxtests.h"
-
-TestResult success_string_literal() {
-    return assert_success("x := \"test\";");
-}
-TestResult success_string_literal_escapes() {
-    return assert_success("x := \"test\\n\";");
-}
